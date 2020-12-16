b8970e2a7410b88f0ac3a6fe08c2d20472475819
I7RANK <franfgh2000@hotmail.com>
2020-12-15 22:27:35 -0500

    Adding one test to states

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,10 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
+import os
+import json
+
+run = os.system
 
 
 class test_state(test_basemodel):
@@ -17,3 +21,13 @@ class test_state(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_aa_create_state(self):
+        """ """
+        no_stdout = " > /dev/null 2>&1"
+        run("rm file.json " + no_stdout)
+        run("echo 'create State' | ./console.py" + no_stdout)
+        run("echo 'all' | ./console.py" + no_stdout)
+        with open("file.json", 'r') as f:
+            temp = json.load(f)
+        self.assertTrue(temp)
