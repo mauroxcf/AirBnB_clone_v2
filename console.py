6e3f24feaeddf45e7828ec70fb0199e0ecd657e0
I7RANK <franfgh2000@hotmail.com>
2020-12-16 17:59:57 -0500

    refactoring __init__ in base_model.py

--- console.py
+++ console.py
@@ -169,8 +169,7 @@ class HBNBCommand(cmd.Cmd):
         if args1[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args1[0]]()
-        new_instance.__dict__.update(new_dict)
+        new_instance = HBNBCommand.classes[args1[0]](**new_dict)
         storage.save()
         print(new_instance.id)
 
