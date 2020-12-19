cd8cd6188248a96790b3c2e1f8ff2266f179f849
mauroxcf <maurox03@gmail.com>
2020-12-19 14:32:05 -0500

    Revert "9. DBStorage - Review, updating review, place and user class"
    This reverts commit 040c76996fe69967b887f733f3bedf856a624fc0.

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" module test of place"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 
 
 class test_Place(test_basemodel):
-    """ all tests case of place"""
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,52 +16,52 @@ class test_Place(test_basemodel):
     def test_city_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.city_id), type(None))
+        self.assertEqual(type(new.city_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), type(None))
+        self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.name), type(None))
+        self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.description), type(None))
+        self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_rooms), type(None))
+        self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), type(None))
+        self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.max_guest), type(None))
+        self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.price_by_night), type(None))
+        self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), type(None))
+        self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.latitude), type(None))
+        self.assertEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
         """ """
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" module test of review"""
+""" """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ all tests case of review"""
+    """ """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -16,14 +16,14 @@ class test_review(test_basemodel):
     def test_place_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), type(None))
+        self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.user_id), type(None))
+        self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.text), type(None))
+        self.assertEqual(type(new.text), str)
