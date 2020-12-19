6104fc9ed3972cdc42ac2a88de50ae5716250a94
mauroxcf <maurox03@gmail.com>
2020-12-19 14:32:09 -0500

    Revert "fixing review.py"
    This reverts commit f40f0e6e5857c6b05aa75f7a0afe23fcb547e695.

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = 'reviews'
+    __tablename__: 'reviews'
     text = Column(String(1024), nullable=True)
     place_id = Column(String(60), nullable=False, ForeignKey('place.id'))
     user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
