e4732f5fcec3c11a67699ab18ae9f990f8076156
mauroxcf <maurox03@gmail.com>
2020-12-19 13:13:08 -0500

    8. DBStorage - Place, update user, place and city

--- models/city.py
+++ models/city.py
@@ -12,3 +12,9 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'))
     name = Column(String(128), nullable=False)
+
+    places = relationship(
+        "Place",
+        backref="cities",
+        cascade="all, delete, delete-orphan"
+    )
