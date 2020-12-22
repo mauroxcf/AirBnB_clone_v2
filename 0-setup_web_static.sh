cca07fffd1b103b6d6286fdf271cbb6bae5584ef
mauroxcf <maurox03@gmail.com>
2020-12-22 18:46:21 -0500

    Bash script that sets up your web servers for the deployment of web_static with folders, symbolic links nad nginx configuration

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,14 @@
 #!/usr/bin/env bash
-# sets up my web servers for the deployment of web_static
-
-sudo apt-get -y update
-sudo apt -y install nginx
-
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-printf '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
-</head>
-<body>
-    Testing Nginx configuration
-</body>
-</html>
-' | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -Rh ubuntu:ubuntu /data/
-
-LOCATION_ALIAS="\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-ARG_SED="/listen 80 default_server;/a \\$LOCATION_ALIAS\n\n"
-echo $ARG_SED
-sudo sed -i "${ARG_SED}" /etc/nginx/sites-available/default
-
-sudo service nginx restart
+#sets up your web servers for the deployment of web_static
+sudo apt -y update
+sudo apt install -y nginx
+mkdir data
+mkdir data/web_static
+mkdir data/web_static/releases
+mkdir data/web_static/shared
+mkdir data/web_static/releases/test
+touch data/web_static/releases/test/index.html && sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/current /data/web_static/releases/test/
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+service nginx restart
