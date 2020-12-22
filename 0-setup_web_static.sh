d9604fde3546fa7f5143b4fa34b4492dbc603025
mauroxcf <maurox03@gmail.com>
2020-12-22 18:53:59 -0500

    Bash script that sets up your web servers for the deployment of web_static with folders, symbolic links nad nginx configuration

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,12 @@
 #sets up your web servers for the deployment of web_static
 sudo apt -y update
 sudo apt install -y nginx
-mkdir -p data/web_static/releases
-mkdir -p data/web_static/shared
-mkdir -p data/web_static/releases/test
-touch data/web_static/releases/test/index.html
+sudo mkdir -p data/web_static/releases
+sudo mkdir -p data/web_static/shared
+sudo mkdir -p data/web_static/releases/test
+sudo touch data/web_static/releases/test/index.html
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo service nginx restart
