932ce72fad24cd8d178430a588f48409c63a8328
mauroxcf <maurox03@gmail.com>
2020-12-22 22:07:09 -0500

    Fabric script that generates a .tgz archive from the contents of the web_static folder

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,8 +12,9 @@ def do_pack():
     now = datetime.now()
     dt_string = now.strftime("%Y%m%d%H%M%S")
     try:
-        local ("mkdir -p versions")
-        local("tar -zcvf versions/web_static_{}.tgz web_static/".format(dt_string))
+        local("mkdir -p versions")
+        local("tar -zcvf versions/web_static_{}.tgz web_static/".format
+              (dt_string))
         return ("versions/{}.tgz".format(compress))
 
     except:
