626b559999514e5955f4867f8d9feb14185c4580
mauroxcf <maurox03@gmail.com>
2020-12-22 23:50:34 -0500

    2. Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Distributes an archive to your
+web servers, using the function do_deploy: """
+
+from fabric.api import *
+from os import path
+
+env.hosts = ['35.237.110.243', '35.237.226.73']
+
+
+def do_deploy(archive_path):
+    """Deploy in the servers"""
+    if not path.exists(archive_path):
+        return False
+
+    ufile = put(archive_path, '/tmp/')
+    if ufile.failed:
+        return False
+
+    complete = archive_path.split("/")[-1]
+    fileok = complete.split(".")[0]
+
+    run("mkdir -p /data/web_static/releases/{}/".format(fileok))
+    run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+        format(fileok, fileok))
+
+    run("rm /tmp/{}.tgz".format(fileok))
+
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/".format(fileok, fileok))
+    run("rm -rf /data/web_static/releases/{}/web_static".
+        format(fileok))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        format(fileok))
+    print("New version deployed!")
+    return True
