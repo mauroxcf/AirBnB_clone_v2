27a50763f3f2426c58b0edd00b1b1f03fc14f271
mauroxcf <maurox03@gmail.com>
2020-12-22 19:54:56 -0500

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,11 @@
 #sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get install -y nginx
-sudo mkdir -p data/web_static/releases
-sudo mkdir -p data/web_static/shared
-sudo mkdir -p data/web_static/releases/test
-sudo touch data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
 sudo echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo service nginx restart
