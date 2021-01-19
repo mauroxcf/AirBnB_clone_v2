021b1836c6b3c9739b49c4cc409b3eb57d693398
mauroxcf <maurox03@gmail.com>
2021-01-19 01:31:15 -0500

    script that starts a Flask web application display 2 messagen depende of the route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" display 2 message module """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """ function display hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def holberton():
+    """ function display hello hbnb"""
+    return "HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
