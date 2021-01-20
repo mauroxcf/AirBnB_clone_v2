e9ae4da16f9f4a2cc907957ea06703682699e2b4
mauroxcf <maurox03@gmail.com>
2021-01-20 16:46:44 -0500

    fixing pep8 in 9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,12 +15,12 @@ def session(exception):
     """
     storage.close()
 
+
 @app.route('/cities_by_states', strict_slashes=False)
 def display_states():
     """ function display a html
     with a list of states
     """
-    #all_state = sorted(storage.all(State).values(), key=lambda x: x.name)
     all_state = storage.all(State)
     return render_template('8-cities_by_states.html', stat=all_state)
 
